8ef1af509be12952690613e6883faf1421079391
Titania <titania.la.regina@gmail.com>
2022-08-28 12:07:24 +0200

    js

---
+++ wedding_website/scrip.js
@@ -0,0 +1,4 @@
+document.getElementById("bubble").innerHTML = `ide jöhet mindenféle rozsa és ha elég sokat írok sample akkor meg is fog látszani hogy szép e.
+ez egy rohadt nagy szövegdoboz lesz a körben valami idézet vagy személyes dolog vagy igazából
+bármi, de vajon jól néz e ki ? talán így fasza lesz, és már csak pár dolgot kell bezsúfolni az
+oldalra`;
